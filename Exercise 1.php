2412f8168ffb939247176f6a68c8400b40054492
M303User <M303User@m303.uc-bcf.edu.ph>
2025-03-06 15:59:21 +0800

    Exercise 1:DONE

---
+++ Exercise 1.php
@@ -0,0 +1,15 @@
+<?php
+$a = 15;
+$b = 7;
+
+$sum = $a + $b;
+$difference = $a - $b;
+$product = $a * $b;
+$quotient = $a / $b;
+
+echo "Sum= " . $sum ."\n ";
+echo "Difference= " . $difference ."\n ";
+echo "Product= "  . $product  ."\n ";
+echo "Quotient= " . $quotient ."\n ";
+
+?>
